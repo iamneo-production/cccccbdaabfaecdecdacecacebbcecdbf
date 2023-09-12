def9adc7631f0865d24f24b3c42d08fa5a982f32
Iamneo <techadmin@iamneo.ai>
2023-09-12 11:20:33 +0000

    Backup Commit

--- dater.ps1
+++ dater.ps1
@@ -1,1 +1,4 @@
-
+$day = read-host "enter your day of birth DD "
+$month = read-host "enter your month of birth MM "
+$year = read-host "enter your year of birth YYYY "
+write-host "$day-$month-$year"
