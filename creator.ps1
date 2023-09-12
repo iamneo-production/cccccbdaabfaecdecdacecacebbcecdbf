841cd02def849b6f2106ecad5ef313e61fd42a3d
Iamneo <techadmin@iamneo.ai>
2023-09-12 11:55:33 +0000

    Backup Commit

--- creator.ps1
+++ creator.ps1
@@ -3,7 +3,14 @@ cd Testing
 new-item SubFolder1 -itemtype directory | chmod 777 SubFolder1
 new-item SubFolder2 -itemtype directory | chmod 777 SubFolder2
 for($i=1;$i -le 6 ; $i++){
-if ($i -lt 4){
-    
-} 
-else
+    if ($i -lt 4){
+        cd SubFolder1
+        new-item test$i.txt -itemtype file | chmod 777 test$i.txt
+        cd ..
+    } 
+    else{
+        cd SubFolder2
+        new-item test$i.txt -itemtype file | chmod 777 test$i.txt
+        cd ..
+    } 
+  
